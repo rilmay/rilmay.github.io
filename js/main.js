cdf06235c087bd31fb35a729dd444746687a3633
rilmay <guzovnikita@gmail.com>
2020-10-13 19:17:00 +0300

    Imported jquery, content improvement

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+console.log('hi');
+$(window).ready(console.log('hi'));
+
+$(window).scroll(function() {
+	// console.log('hello');
+	// console.log($(window).scrollTop());	
+	// console.log($(window).height());
+    if($(window).scrollTop() == $(window).height()) {
+    	$('body').append('<div>Hello its popup</div>');
+    	console.log('hi')
+           // ajax call get data from server and append to the div
+    }
+});
+
+$( document ).click(function() {
+	console.log('hello');
+    $( "#toggle2" ).toggle( "bounce", { times: 1 }, "slow" );
+});

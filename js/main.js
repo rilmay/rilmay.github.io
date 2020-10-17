e130f9702294888e09047b9256025474c7fe079d
rilmay <guzovnikita@gmail.com>
2020-10-17 10:49:15 +0300

    Completed about section

--- js/main.js
+++ js/main.js
@@ -8,21 +8,58 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-console.log('hi');
-$(window).ready(console.log('hi'));
+let map = new Map([
+	[".nguzov_message_pack_2", {
+		scrollTopRateCondition: function(scrollTopRate){return scrollTopRate > 1.5 && !this.isShown},
+		resultStyle: {'marginTop': '40px', opacity: '1',}
+	}],
+	[".nguzov_message_pack_1",{
+		scrollTopRateCondition: function(scrollTopRate){return scrollTopRate < 0.2 && !this.isShown},
+		resultStyle: {'marginTop': '220px', opacity: '1',}
+	}]
+]);
 
-$(window).scroll(function() {
-	// console.log('hello');
-	// console.log($(window).scrollTop());	
-	// console.log($(window).height());
-    if($(window).scrollTop() == $(window).height()) {
-    	$('body').append('<div>Hello its popup</div>');
-    	console.log('hi')
-           // ajax call get data from server and append to the div
-    }
-});
 
-$( document ).click(function() {
+const messageAnimation = function(){
+	return function(){
+		let yOffset = ($(window).scrollTop())? $(window).scrollTop(): window.pageYOffset;
+		let scrollTopRate = yOffset / $(window).height();
+
+		map.forEach(function(value, key, map){
+			if(value.scrollTopRateCondition(scrollTopRate)){
+				$(key).animate(value.resultStyle, 1000);
+				value.isShown = true;
+			}
+		});
+	}
+}
+
+$(window).ready(messageAnimation());
+$(window).scroll(messageAnimation());
+
+// $(window).scroll(function() {
+// 	let scrollTopRate = $(window).scrollTop() / $(window).height();
+// 	if(scrollTopRate > 1.4 && !isShown){
+// 		$('.nguzov_message_pack_2').animate({
+// 			'marginTop': '40px',
+// 			opacity: '1',
+// 		}, 1000);
+// 		// $('.nguzov_message_pack_2').fadeTo("slow", 1);
+// 		isShown = true;
+// 	}
+// });
+
+
+$( "#toggle2" ).click(function() {
 	console.log('hello');
     $( "#toggle2" ).toggle( "bounce", { times: 1 }, "slow" );
 });
+
+
+// $( "#avatar" ).hover(function(){
+// 	// $( "#avatar" ).animate({width: (currentAvatarWandH * 1.05) + 'px', height: (currentAvatarWandH * 1.05) + 'px'}, 500);
+// 	$( "#avatar" ).css({ transform: 'scale(1.005)' });
+// }, function(){
+// 	$( "#avatar" ).css({ transform: 'scale(0.995)' });
+// }
+// );
